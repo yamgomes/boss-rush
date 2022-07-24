aeed5b1abea7b9ce866955a15e399de5d35ce8bd
Yam Gomes <yamgcunha@gmail.com>
2022-07-24 00:13:39 -0300

    added sound

--- Assets/Scripts/Activator.cs
+++ Assets/Scripts/Activator.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Activator : MonoBehaviour
 {
+    public AudioSource audioSource;
+    public AudioClip clip;
+    public float volume = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
-
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = clip;
+        audioSource.volume = volume;
     }
 
     // Update is called once per frame
@@ -21,7 +26,9 @@ public class Activator : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             GameManager.GetInstance().DealDamage();
+            audioSource.PlayOneShot(clip, volume);
         }
+
     }
 
 }
